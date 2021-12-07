48dafc49c0addeb5bb78cd3e8e3810c62e882c02
Felipe <felipenovaes@ufl.edu>
2021-12-06 20:31:17 -0500

    Here we go!

--- WikiRaceSolver/WikiRaceSolver.cpp
+++ WikiRaceSolver/WikiRaceSolver.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hey guys!\n";
-    std::cout << "Testing commit\n";
-    std::cout << "Hey back!\n";
-    std::cout << "More testing\n";
+int readVertex(string name) {
+	int vertex;
+	string temp;
+	ifstream iFile("SampleData/SampleVertices.txt");
+	while (iFile) {
+		iFile >> vertex;
+		iFile >> name;
+		if (temp == name) {
+			return vertex;
+		}
+	}
+}
+
+vector<int> readEdges(int vertex) {
+	int from;
+	int to;
+	vector<int> edges;
+	ifstream iFile("SampleData/SampleEdges.txt");
+	while (iFile) {
+		iFile >> from;
+		iFile >> to;
+		if (from == vertex) {
+			edges.push_back(to);
+		}
+	}
+	return edges;
+}
+
+int main() {
+	cout << "Hello World!\n";
 }
