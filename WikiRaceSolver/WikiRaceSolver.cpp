e4b45bccfae37961709ff7c29092e667cddf0173
Felipe <felipenovaes@ufl.edu>
2021-12-07 18:11:40 -0500

    Template.

--- WikiRaceSolver/WikiRaceSolver.cpp
+++ WikiRaceSolver/WikiRaceSolver.cpp
@@ -5,7 +5,7 @@
 #include <sstream>
 using namespace std;
 
-int readVertex(string name) {
+int readVertexID(string name) {
 	int vertex;
 	string temp;
 	ifstream iFile("SampleData/SampleVertices.txt");
@@ -18,19 +18,29 @@ int readVertex(string name) {
 	iFile.close();
 }
 
+string readVertexName(int ID) {
+	int vertex;
+	string temp;
+	ifstream iFile("SampleData/SampleVertices.txt");
+	while (iFile) {
+		iFile >> vertex;
+		getline(iFile, temp);
+		if (vertex == ID)
+			return temp.substr(2, temp.size() - 3);
+	}
+	iFile.close();
+}
+
 vector<int> readEdges(int vertex) {
-	int first;
-	int second;
+	int from;
+	int to;
 	vector<int> edges;
 	ifstream iFile("SampleData/SampleEdges.txt");
 	while (iFile) {
-		iFile >> first;
-		iFile >> second;
-		if (first == vertex) {
-			edges.push_back(second);
-		}
-		if (second == vertex) {
-			edges.push_back(first);
+		iFile >> from;
+		iFile >> to;
+		if (from == vertex) {
+			edges.push_back(to);
 		}
 	}
 	iFile.close();
@@ -39,7 +49,10 @@ vector<int> readEdges(int vertex) {
 
 int main() {
 	string exName = "Alexander Seton (d. 1332)";
-	int exVertex = readVertex(exName);
+	int exVertex = readVertexID(exName);
 	vector<int> exEdges = readEdges(exVertex);
-	cout << "This vertex: " << exName << " has " << exEdges.size() << " edges." << endl;
+	cout << "This vertex: " << exName << " has these following edges: " << endl;
+	for (int i = 0; i < exEdges.size(); i++) {
+		cout << readVertexName(exEdges[i]) << endl;
+	}
 }
