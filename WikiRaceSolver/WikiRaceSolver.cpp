5ba31c4c1b28bcd786bca69db287181d2fc88958
Felipe <felipenovaes@ufl.edu>
2021-12-08 15:39:16 -0500

    edit

--- WikiRaceSolver/WikiRaceSolver.cpp
+++ WikiRaceSolver/WikiRaceSolver.cpp
@@ -62,7 +62,7 @@ string readVertexName(int ID) {
     iFile.close();
 }
 
-vector<int> readEdges(int vertex, AdjacencyList &adjList) {
+vector<int> readEdges(int vertex) {
     int from;
     int to;
     vector<int> edges;
@@ -70,7 +70,6 @@ vector<int> readEdges(int vertex, AdjacencyList &adjList) {
     while (iFile) {
         iFile >> from;
         iFile >> to;
-        adjList.addEdge(from, to);
         if (from == vertex) {
             edges.push_back(to);
         }
@@ -80,10 +79,9 @@ vector<int> readEdges(int vertex, AdjacencyList &adjList) {
 }
 
 int main() {
-    AdjacencyList exAdjList(100000);
     string exName = "Alexander Seton (d. 1332)";
     int exVertex = readVertexID(exName);
-    vector<int> exEdges = readEdges(exVertex, exAdjList);
+    vector<int> exEdges = readEdges(exVertex);
     cout << "This vertex: " << exName << " has these following edges: " << endl;
     for (int i = 0; i < exEdges.size(); i++) {
         cout << readVertexName(exEdges[i]) << endl;
