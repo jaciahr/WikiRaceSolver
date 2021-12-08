a06e5fe132bbbbf2ae8080f8fe8d86845d8d8b0f
jacia <jacia@DESKTOP-LDN9JIH>
2021-12-08 15:22:11 -0500

    Adjacency List edited

--- WikiRaceSolver/WikiRaceSolver.cpp
+++ WikiRaceSolver/WikiRaceSolver.cpp
@@ -78,9 +78,10 @@ vector<int> readEdges(int vertex, AdjacencyList adjList) {
 }
 
 int main() {
+    AdjacencyList exAdjList(100000);
     string exName = "Alexander Seton (d. 1332)";
     int exVertex = readVertexID(exName);
-    vector<int> exEdges = readEdges(exVertex);
+    vector<int> exEdges = readEdges(exVertex, exAdjList);
     cout << "This vertex: " << exName << " has these following edges: " << endl;
     for (int i = 0; i < exEdges.size(); i++) {
         cout << readVertexName(exEdges[i]) << endl;
