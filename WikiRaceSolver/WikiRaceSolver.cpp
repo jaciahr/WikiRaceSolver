4d1f58ac66414763c7de0b12659650393a1c0cab
Felipe <felipenovaes@ufl.edu>
2021-12-08 15:08:41 -0500

    Edge List.

--- WikiRaceSolver/WikiRaceSolver.cpp
+++ WikiRaceSolver/WikiRaceSolver.cpp
@@ -2,47 +2,29 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <sstream>
+#include <unordered_map>
 using namespace std;
 
-class Graph
-{
-private:
-    int numVertices;
-    bool** matrix;
+struct elgGraph {
+    vector<pair<int, int>> elg;
 
-public:
-    Graph(int numVertices) {
-        this->numVertices = numVertices;
-        matrix = new bool* [numVertices];
-        for (int i = 0; i < numVertices; i++) {
-            matrix[i] = new bool[numVertices];
-            for (int j = 0; j < numVertices; j++)
-                matrix[i][j] = false;
+    void MakeELG() {
+        int from, to;
+        ifstream iFile("SampleData/SampleEdges.txt");
+        while (iFile) {
+            iFile >> from;
+            iFile >> to;
+            this->elg.push_back(make_pair(from, to));
         }
+        iFile.close();
     }
-    void insertEdge(int from, int to) {
-        matrix[from][to] = true;
-    }
-    void removeEdge(int from, int to) {
-        matrix[from][to] = false;
-    }
-    bool isEdge(int from, int to) {
-        return matrix[from][to];
-    }
-    void printMatrix() {
-        for (int i = 0; i < numVertices; i++) {
-            for (int j = 0; j < numVertices; j++)
-                cout << matrix[i][j] << " ";
-            cout << endl;
-        }
-    }
+
 };
 
 int readVertexID(string name) {
     int vertex;
     string temp;
-    ifstream iFile("SampleData/SampleVertices.txt");
+    ifstream iFile("SampleData/SampleNames.txt");
     while (iFile) {
         iFile >> vertex;
         getline(iFile, temp);
@@ -55,7 +37,7 @@ int readVertexID(string name) {
 string readVertexName(int ID) {
     int vertex;
     string temp;
-    ifstream iFile("SampleData/SampleVertices.txt");
+    ifstream iFile("SampleData/SampleNames.txt");
     while (iFile) {
         iFile >> vertex;
         getline(iFile, temp);
@@ -65,9 +47,8 @@ string readVertexName(int ID) {
     iFile.close();
 }
 
-vector<int> readEdges(int vertex, Graph matrix) {
-    int from;
-    int to;
+vector<int> readEdges(int vertex) {
+    int from, to;
     vector<int> edges;
     ifstream iFile("SampleData/SampleEdges.txt");
     while (iFile) {
@@ -75,15 +56,6 @@ vector<int> readEdges(int vertex, Graph matrix) {
         iFile >> to;
         if (from == vertex) {
             edges.push_back(to);
-            matrix.insertEdge(from, to);
-            /* NOTE: The above line should actually be above the if statement.
-               The issue is that the ID's of the vertices are not equivalent
-               to their line number. If you were to allocate a 2D array 
-               to the ID of the last vertex (i.e., 4203055 x 4203055), it
-               would be too large (at least for my PC's specs, since that's
-               what the heap is dependent on). Should we rename vertex ID's
-               to their appropriate line number? 
-            */
         }
     }
     iFile.close();
@@ -91,14 +63,11 @@ vector<int> readEdges(int vertex, Graph matrix) {
 }
 
 int main() {
-    Graph exMatrix(41329);
     string exName = "Alexander Seton (d. 1332)";
     int exVertex = readVertexID(exName);
-    vector<int> exEdges = readEdges(exVertex, exMatrix);
+    vector<int> exEdges = readEdges(exVertex);
     cout << "This vertex: " << exName << " has these following edges: " << endl;
     for (int i = 0; i < exEdges.size(); i++) {
         cout << readVertexName(exEdges[i]) << endl;
     }
-    //This is obviously huge so you're just going to be spammed with 0's lol
-    //exMatrix.printMatrix();
 }
