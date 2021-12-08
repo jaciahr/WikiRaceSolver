2c9aac81b914fa8a4f927e81d691ede464059f91
jacia <jacia@DESKTOP-LDN9JIH>
2021-12-08 15:19:36 -0500

    Added adjacency list

--- WikiRaceSolver/WikiRaceSolver.cpp
+++ WikiRaceSolver/WikiRaceSolver.cpp
@@ -5,6 +5,19 @@
 #include <unordered_map>
 using namespace std;
 
+class AdjacencyList {
+    int numVertices;
+    list<int>* adjLists;
+
+public:
+    AdjacencyList(int numVertices) {
+        this->numVertices = numVertices;
+    }
+    void addEdge(int src, int dest) {
+        adjLists[src].push_back(dest);
+    }
+};
+
 struct elgGraph {
     vector<pair<int, int>> elg;
 
@@ -47,13 +60,15 @@ string readVertexName(int ID) {
     iFile.close();
 }
 
-vector<int> readEdges(int vertex) {
-    int from, to;
+vector<int> readEdges(int vertex, AdjacencyList adjList) {
+    int from;
+    int to;
     vector<int> edges;
     ifstream iFile("SampleData/SampleEdges.txt");
     while (iFile) {
         iFile >> from;
         iFile >> to;
+        adjList.addEdge(from, to);
         if (from == vertex) {
             edges.push_back(to);
         }
