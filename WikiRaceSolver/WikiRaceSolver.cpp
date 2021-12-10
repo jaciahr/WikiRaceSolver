d78ea2801132d279920f8bf2f595dec719779c32
Felipe <felipenovaes@ufl.edu>
2021-12-09 19:10:49 -0500

    WikiRaceSolver!

--- WikiRaceSolver/WikiRaceSolver.cpp
+++ WikiRaceSolver/WikiRaceSolver.cpp
@@ -185,119 +185,132 @@ struct elgGraph {
     }
 };
 
-int readVertexID(string name) {
+void readVertices(map<int, string> &key, map<string, int> &reversekey) {
     int vertex;
     string temp;
     ifstream iFile("SampleData/SampleNames.txt");
     while (iFile) {
         iFile >> vertex;
         getline(iFile, temp);
-        if (temp.substr(2, temp.size() - 3) == name) {
-            iFile.close();
-            return vertex;
-        }
+        temp = temp.substr(2, temp.size() - 3);
+        key[vertex] = temp;
+        reversekey[temp] = vertex;
     }
     iFile.close();
-    return -1;
-}
-
-string readVertexName(int ID) {
-    int vertex;
-    string temp;
-    ifstream iFile("SampleData/SampleNames.txt");
-    while (iFile) {
-        iFile >> vertex;
-        getline(iFile, temp);
-        if (vertex == ID) {
-            iFile.close();
-            return temp.substr(2, temp.size() - 3);
-        }
-    }
-    iFile.close();
-    return "";
 }
 
 int main() {
     AdjacencyList aGraph;
     elgGraph eGraph;
+    map<int, string> key;
+    map<string, int> reversekey;
+    bool aGraphCheck = false, eGraphCheck = false, keyCheck = false;
+    string options = "";
 
-    int startID, targetID;
-    string options, start, target;
-    cout << "Choose one of the options below:" << endl;
-    cout << "1: Adjacency List Graph with BFS" << endl;
-    cout << "2: Adjacency List Graph with DFS" << endl;
-    cout << "3: Edge List Graph with BFS" << endl;
-    cout << "4: Edge List Graph with DFS" << endl;
-    getline(cin, options);
-    if (options != "1" && options != "2" && options != "3" && options != "4") {
-        cout << "Invalid option choice." << endl;
-        return 0;
-    }
-    cout << "Input name of starting site." << endl;
-    getline(cin, start);
-    startID = readVertexID(start);
-    if (startID == -1) {
-        cout << "That site does not exist within the database." << endl;
-        return 0;
-    }
-    cout << "Input name of target site." << endl;
-    getline(cin, target);
-    targetID = readVertexID(target);
-    if (targetID == -1) {
-        cout << "That site does not exist within the database." << endl;
-        return 0;
-    }
+    while (options != "5") {
+        string start, target;
+        int startID, targetID;
+        cout << "Choose one of the options below:" << endl;
+        cout << "1: Adjacency List Graph with BFS" << endl;
+        cout << "2: Adjacency List Graph with DFS" << endl;
+        cout << "3: Edge List Graph with BFS" << endl;
+        cout << "4: Edge List Graph with DFS" << endl;
+        cout << "5: Exit" << endl;
+        getline(cin, options);
+        if (options != "1" && options != "2" && options != "3" && options != "4") {
+            if (options == "5")
+                return 0;
+            cout << "Invalid option choice." << endl;
+            continue;
+        }
+        if (!keyCheck) {
+            keyCheck = true;
+            readVertices(key, reversekey);
+        }
+        cout << "Input name of starting site." << endl;
+        getline(cin, start);
+        if (!reversekey.count(start)) {
+            cout << "That site does not exist within the database." << endl;
+            continue;
+        }
+        startID = reversekey[start];
+        cout << "Input name of target site." << endl;
+        getline(cin, target);
+        if (!reversekey.count(target)) {
+            cout << "That site does not exist within the database." << endl;
+            continue;
+        }
+        targetID = reversekey[target];
 
-    if (options == "1") {
-        aGraph.makeAdjList();
-        aGraph.BFS(startID, targetID);
-        if (aGraph.results.size() == 0) {
-            cout << "There is no path between these sites." << endl;
-            return 0;
+        if (options == "1") {
+            if (!aGraphCheck) {
+                aGraphCheck = true;
+                aGraph.makeAdjList();
+            }
+            aGraph.BFS(startID, targetID);
+            if (aGraph.results.size() == 0) {
+                cout << "There is no path between these sites." << endl;
+                continue;
+            }
+            cout << "There are " << aGraph.results.size() - 1 << " degrees of separation." << endl;
+            cout << "The shortest path is: ";
+            for (int i = aGraph.results.size() - 1; i > 0; i--)
+                cout << key[aGraph.results[i]] << ", ";
+            cout << key[aGraph.results[0]] << "." << endl;
         }
-        cout << "There are " << aGraph.results.size() << " degrees of separation." << endl;
-        cout << "The shortest path is: ";
-        for (int i = aGraph.results.size() - 1; i > 0; i--)
-            cout << readVertexName(aGraph.results[i]) << ", ";
-        cout << readVertexName(aGraph.results[0]) << "." << endl;
-    }
-    else if (options == "2") {
-        aGraph.makeAdjList();
-        aGraph.DFS(startID, targetID);
-        if (aGraph.results.size() == 0) {
-            cout << "There is no path between these sites." << endl;
-            return 0;
+        else if (options == "2") {
+            if (!aGraphCheck) {
+                aGraphCheck = true;
+                aGraph.makeAdjList();
+            }
+            aGraph.DFS(startID, targetID);
+            if (aGraph.results.size() == 0) {
+                cout << "There is no path between these sites." << endl;
+                continue;
+            }
+            cout << "There are " << aGraph.results.size() - 1 << " degrees of separation." << endl;
+            cout << "The shortest path is: ";
+            for (int i = aGraph.results.size() - 1; i > 0; i--)
+                cout << key[aGraph.results[i]] << ", ";
+            cout << key[aGraph.results[0]] << "." << endl;
         }
-        cout << "There are " << aGraph.results.size() << " degrees of separation." << endl;
-        cout << "The shortest path is: ";
-        for (int i = aGraph.results.size() - 1; i > 0; i--)
-            cout << readVertexName(aGraph.results[i]) << ", ";
-        cout << readVertexName(aGraph.results[0]) << "." << endl;
-    }
-    else if (options == "3") {
-        eGraph.MakeELG();
-        eGraph.BFS(startID, targetID);
-        if (eGraph.results.size() == 0) {
-            cout << "There is no path between these sites." << endl;
-            return 0;
+        else if (options == "3") {
+            if (!eGraphCheck) {
+                eGraphCheck = true;
+                eGraph.MakeELG();
+            }
+            eGraph.BFS(startID, targetID);
+            if (eGraph.results.size() == 0) {
+                cout << "There is no path between these sites." << endl;
+                continue;
+            }
+            cout << "There are " << eGraph.results.size() - 1 << " degrees of separation." << endl;
+            cout << "The shortest path is: ";
+            for (int i = 0; i < eGraph.results.size() - 1; i++)
+                cout << key[eGraph.results[i]] << ", ";
+            cout << key[eGraph.results[eGraph.results.size() - 1]] << "." << endl;
         }
-        cout << "There are " << eGraph.results.size() << " degrees of separation." << endl;
-        cout << "The shortest path is: ";
-        for (int i = 0; i < eGraph.results.size() - 1; i++)
-            cout << readVertexName(eGraph.results[i]) << ", ";
-        cout << readVertexName(eGraph.results[eGraph.results.size() - 1]) << "." << endl;
-    }
-    else if (options == "4") {
-        eGraph.MakeELG();
-        eGraph.DFS(startID, targetID);
-        if (eGraph.results.size() == 0) {
-            cout << "There is no path between these sites." << endl;
-            return 0;
+        else if (options == "4") {
+            if (!eGraphCheck) {
+                eGraphCheck = true;
+                eGraph.MakeELG();
+            }
+            eGraph.DFS(startID, targetID);
+            if (eGraph.results.size() == 0) {
+                cout << "There is no path between these sites." << endl;
+                continue;
+            }
+            cout << "There are " << eGraph.results.size() - 1 << " degrees of separation." << endl;
+            cout << "The shortest path is: ";
+            for (int i = 0; i < eGraph.results.size() - 1; i++)
+                cout << key[eGraph.results[i]] << ", ";
+            cout << key[eGraph.results[eGraph.results.size() - 1]] << "." << endl;
         }
-        cout << "There are " << eGraph.results.size() << " degrees of separation." << endl;
-        cout << "The shortest path is: ";
-        for (int i = 0; i < eGraph.results.size() - 1; i++)
-            cout << readVertexName(eGraph.results[i]) << ", ";
-        cout << readVertexName(eGraph.results[eGraph.results.size() - 1]) << "." << endl;
+        aGraph.visited.clear();
+        aGraph.prevs.clear();
+        aGraph.results.clear();
+        eGraph.visited.clear();
+        eGraph.prevs.clear();
+        eGraph.results.clear();
     }
 }
